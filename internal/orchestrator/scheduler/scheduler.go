750dd4858ae14268d5f17a06f95ceeaafde52a4d
Paulo Ricardo <plricardo504@gmail.com>
2025-09-16 13:49:15 -0300

    feat: add barebones interfaces of strategies

--- internal/orchestrator/scheduler/scheduler.go
+++ internal/orchestrator/scheduler/scheduler.go
@@ -2,53 +2,64 @@ package scheduler
 
 import (
 	"fmt"
+	"sort"
 
 	"github.com/svaan1/tcc-go/internal/orchestrator/jq"
 	"github.com/svaan1/tcc-go/internal/orchestrator/np"
 )
 
-// NodeScheduler implements the strategy for choosing optimal nodes
-type NodeScheduler struct {
-	strategy SelectionStrategy
+type JobScheduler struct {
+	jp JobPriorityStrategy
+	ns NodeSelectionStrategy
 }
 
-func NewNodeScheduler(strategy SelectionStrategy) *NodeScheduler {
-	return &NodeScheduler{
-		strategy: strategy,
+func NewJobScheduler(jp JobPriorityStrategy, ns NodeSelectionStrategy) *JobScheduler {
+	return &JobScheduler{
+		jp: jp,
+		ns: ns,
 	}
 }
 
-func (ns *NodeScheduler) SelectBestNode(job *jq.Job, availableNodes []*np.Node) (*NodeScore, error) {
-	var bestScore *NodeScore
-
-	for _, node := range availableNodes {
-		nodeScore := ns.strategy.ScoreNode(job, node)
-
-		if bestScore == nil || nodeScore.Score > bestScore.Score {
-			bestScore = nodeScore
-		}
+func (js *JobScheduler) RankJobs(jobs []*jq.Job) ([]JobPriorityScore, error) {
+	var scores []JobPriorityScore
+	for _, job := range jobs {
+		scores = append(scores, js.jp.ScoreJobPriority(job))
 	}
 
-	if bestScore == nil {
-		return nil, fmt.Errorf("could not find any node for the job")
-	}
-
-	return bestScore, nil
-}
-
-func (ns *NodeScheduler) RankNodes(job *jq.Job, nodes []*np.Node) ([]*NodeScore, error) {
-	var scores []*NodeScore
-	for _, node := range nodes {
-		scores = append(scores, ns.strategy.ScoreNode(job, node))
-	}
+	sort.Slice(scores, func(i, j int) bool {
+		return scores[i].Score > scores[j].Score
+	})
 
 	return scores, nil
 }
 
-func (ns *NodeScheduler) SetStrategy(strategy SelectionStrategy) {
-	ns.strategy = strategy
+func (js *JobScheduler) RankNodes(job *jq.Job, nodes []*np.Node) ([]NodeScore, error) {
+	var scores []NodeScore
+	for _, node := range nodes {
+		scores = append(scores, js.ns.ScoreNode(job, node))
+	}
+
+	sort.Slice(scores, func(i, j int) bool {
+		return scores[i].Score > scores[j].Score
+	})
+
+	return scores, nil
 }
 
-func (ns *NodeScheduler) GetStrategy() SelectionStrategy {
-	return ns.strategy
+func (js *JobScheduler) SelectBestNode(job *jq.Job, availableNodes []*np.Node) (NodeScore, error) {
+	var bestScore NodeScore
+
+	for _, node := range availableNodes {
+		nodeScore := js.ns.ScoreNode(job, node)
+
+		if bestScore.Node == nil || nodeScore.Score > bestScore.Score {
+			bestScore = nodeScore
+		}
+	}
+
+	if bestScore.Node == nil {
+		return NodeScore{}, fmt.Errorf("could not find any node for the job")
+	}
+
+	return bestScore, nil
 }
--- internal/orchestrator/scheduler/strategy.go
+++ internal/orchestrator/scheduler/strategy.go
@@ -5,11 +5,20 @@ import (
 	"github.com/svaan1/tcc-go/internal/orchestrator/np"
 )
 
+type JobPriorityScore struct {
+	Job   *jq.Job `json:"job"`
+	Score float64 `json:"score"`
+}
+
 type NodeScore struct {
 	Node  *np.Node `json:"node"`
 	Score float64  `json:"score"` // Higher is better
 }
 
-type SelectionStrategy interface {
-	ScoreNode(job *jq.Job, node *np.Node) *NodeScore
+type JobPriorityStrategy interface {
+	ScoreJobPriority(job *jq.Job) JobPriorityScore
+}
+
+type NodeSelectionStrategy interface {
+	ScoreNode(job *jq.Job, node *np.Node) NodeScore
 }
