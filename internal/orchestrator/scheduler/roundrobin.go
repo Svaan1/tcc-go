bc76b9208070529e6b4c866d616ddba71aa0ab9e
Paulo Ricardo <plricardo504@gmail.com>
2025-09-16 14:01:47 -0300

    refactor: change the scheduler interface and implement rr

---
+++ internal/orchestrator/scheduler/roundrobin.go
@@ -0,0 +1,39 @@
+package scheduler
+
+import (
+	"context"
+	"fmt"
+	"sync"
+
+	"github.com/svaan1/tcc-go/internal/orchestrator/jq"
+	"github.com/svaan1/tcc-go/internal/orchestrator/np"
+)
+
+type RoundRobinScheduler struct {
+	nextIdx int
+
+	mu sync.Mutex
+}
+
+func (rr *RoundRobinScheduler) RankNodes(ctx context.Context, job *jq.Job, nodes []*np.Node) ([]*np.Node, error) {
+	// Round-robin does not rank nodes, so we return the original list.
+	return nodes, nil
+}
+
+func (rr *RoundRobinScheduler) SelectBestNode(job *jq.Job, availableNodes []*np.Node) (*np.Node, error) {
+	rr.mu.Lock()
+	defer rr.mu.Unlock()
+
+	if len(availableNodes) == 0 {
+		return nil, fmt.Errorf("no available nodes")
+	}
+
+	if rr.nextIdx >= len(availableNodes) {
+		rr.nextIdx = 0
+	}
+
+	bestNode := availableNodes[rr.nextIdx]
+	rr.nextIdx++
+
+	return bestNode, nil
+}
--- internal/orchestrator/scheduler/scheduler.go
+++ internal/orchestrator/scheduler/scheduler.go
@@ -1,65 +1,13 @@
 package scheduler
 
 import (
-	"fmt"
-	"sort"
+	"context"
 
 	"github.com/svaan1/tcc-go/internal/orchestrator/jq"
 	"github.com/svaan1/tcc-go/internal/orchestrator/np"
 )
 
-type JobScheduler struct {
-	jp JobPriorityStrategy
-	ns NodeSelectionStrategy
-}
-
-func NewJobScheduler(jp JobPriorityStrategy, ns NodeSelectionStrategy) *JobScheduler {
-	return &JobScheduler{
-		jp: jp,
-		ns: ns,
-	}
-}
-
-func (js *JobScheduler) RankJobs(jobs []*jq.Job) ([]JobPriorityScore, error) {
-	var scores []JobPriorityScore
-	for _, job := range jobs {
-		scores = append(scores, js.jp.ScoreJobPriority(job))
-	}
-
-	sort.Slice(scores, func(i, j int) bool {
-		return scores[i].Score > scores[j].Score
-	})
-
-	return scores, nil
-}
-
-func (js *JobScheduler) RankNodes(job *jq.Job, nodes []*np.Node) ([]NodeScore, error) {
-	var scores []NodeScore
-	for _, node := range nodes {
-		scores = append(scores, js.ns.ScoreNode(job, node))
-	}
-
-	sort.Slice(scores, func(i, j int) bool {
-		return scores[i].Score > scores[j].Score
-	})
-
-	return scores, nil
-}
-
-func (js *JobScheduler) SelectBestNode(job *jq.Job, availableNodes []*np.Node) (NodeScore, error) {
-	var bestScore NodeScore
-
-	for _, node := range availableNodes {
-		nodeScore := js.ns.ScoreNode(job, node)
-
-		if bestScore.Node == nil || nodeScore.Score > bestScore.Score {
-			bestScore = nodeScore
-		}
-	}
-
-	if bestScore.Node == nil {
-		return NodeScore{}, fmt.Errorf("could not find any node for the job")
-	}
-
-	return bestScore, nil
+type JobScheduler interface {
+	RankNodes(ctx context.Context, job *jq.Job, nodes []*np.Node) ([]*np.Node, error)
+	SelectBestNode(job *jq.Job, availableNodes []*np.Node) (*np.Node, error)
 }
